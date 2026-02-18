59bc7180ba74dea0bd3f91d9a5a4ca36c3639074
moogman <moogman@gmail.com>
2026-02-18 22:06:37 +0000

    Guard SpeechAnalyzer provider creation behind macOS 26 availability

--- Sources/FluidSpeechAnalyzerPlugin/FluidSpeechAnalyzerPluginEntryPoint.swift
+++ Sources/FluidSpeechAnalyzerPlugin/FluidSpeechAnalyzerPluginEntryPoint.swift
@@ -4,7 +4,10 @@ import Foundation
 public final class FluidSpeechAnalyzerPluginEntryPoint: SpeechAnalyzerPluginEntryPoint {
     public static func makeProvider(logSink: (any SpeechAnalyzerPluginLogger)?) -> (any SpeechAnalyzerProvider)? {
         #if ENABLE_SPEECH_ANALYZER
-        return AppleSpeechAnalyzerProvider(logSink: logSink)
+        if #available(macOS 26.0, *) {
+            return AppleSpeechAnalyzerProvider(logSink: logSink)
+        }
+        return nil
         #else
         return nil
         #endif
